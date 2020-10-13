72886a47e5a3082982fb39484eebdd6c96f9ef8a
alexsincai <alex.sincai@yahoo.co.uk>
2020-10-13 15:13:11 +0300

    building list

--- script.js
+++ script.js
@@ -10,8 +10,10 @@ function saveData() {
 }
 
 //  mark done
+function markDone(i) {}
 
 //  delete
+function deleteEntry(i) {}
 
 //  add item
 function addItem(event) {
@@ -25,12 +27,48 @@ function addItem(event) {
 
     textField.value = "";
     saveData();
+    buildList();
 }
 
 //  build list
+function buildList() {
+    let list = document.querySelector("#list");
+    list.innerHTML = "";
+
+    for (let i = 0; i < items.length; i++) {
+        let div = document.createElement("div");
+        if (items[i].done) {
+            div.classList.add("done");
+        }
+
+        let span = document.createElement("span");
+        span.innerText = items[i].text;
+
+        let markButton = document.createElement("button");
+        markButton.innerText = "Done?";
+        markButton.addEventListener("click", function (event) {
+            event.preventDefault();
+            markDone(i);
+        });
+
+        let deleteButton = document.createElement("button");
+        deleteButton.innerText = "Delete";
+        deleteButton.addEventListener("click", function (event) {
+            event.preventDefault();
+            deleteEntry(i);
+        });
+
+        div.appendChild(span);
+        div.appendChild(markButton);
+        div.appendChild(deleteButton);
+
+        list.appendChild(div);
+    }
+}
 
 //  interactions
 document.querySelector("form").addEventListener("submit", addItem);
 
 //  start
 let items = loadData();
+buildList();
